9a12ba43d12552f3f279e28e3701ef6c7036c35d
Yuki Sato <yuki.sato@lightcurve.io>
2021-03-30 16:17:01 +0200

    Add firebase

--- App.js
+++ App.js
@@ -2,8 +2,17 @@ import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 
+import fb from './firebase';
+
 import HomeScreen from './screen/HomeScreen';
 import BikeDetailScreen from './screen/BikeDetailScreen';
+import bikes from './data/bikeArray';
+
+// Adding bike data to 'bikes'collection
+bikes.forEach((data) => 
+  fb.firestore().collection('bikes').add(data)
+)
+//
 
 const Stack = createStackNavigator();
 
