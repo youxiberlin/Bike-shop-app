a89487789eb2fac71ecd1bf0ae0083001bb785fe
Yuki Sato <yuki.sato@lightcurve.io>
2021-03-30 16:17:14 +0200

    Add array bike data

---
+++ data/bikeArray.js
@@ -0,0 +1,146 @@
+module.exports = [
+  {
+    id: 1,
+    name: "VRC Evo mat black",
+    category: "road",
+    price: 890.50,
+    size: {
+      51: 0,
+      53: 2,
+      55: 0,
+      57: 1,
+      58: 0,
+    },
+    images: [
+      "https://images.internetstores.de/products/1225449/02/372fe5/votec-vrc-evo-black-matte-1.jpg?forceSize=true&forceAspectRatio=true&useTrim=true&size=613x613",
+      "https://images.internetstores.de/products/1225449/01/70c935/votec-vrc-evo-black-matte-2.jpg?forceSize=true&forceAspectRatio=true&useTrim=true&size=613x613",
+      "https://images.internetstores.de/products/1225449/01/0184bc/votec-vrc-evo-black-matte-3.jpg?forceSize=true&forceAspectRatio=true&useTrim=true&size=613x613"
+    ]
+  },
+  {
+    id: 2,
+    name: "Loft 7D EQ Damen seafoam",
+    category: "city",
+    price: 230.50,
+    size: {
+      51: 1,
+      53: 2,
+      55: 0,
+      57: 0,
+      58: 0
+    },
+    images: [
+      "https://images.internetstores.de/products/1076423/02/9f4678/electra-loft-7d-eq-damen-seafoam-7.jpg?forceSize=true&forceAspectRatio=true&useTrim=true&size=613x613",
+      "https://images.internetstores.de/products/1076423/02/b3220d/electra-loft-7d-eq-damen-seafoam-1.jpg?forceSize=true&forceAspectRatio=true&useTrim=true&size=613x613",
+      "https://images.internetstores.de/products/1076423/01/84e570/electra-loft-7d-eq-damen-seafoam-2.jpg?forceSize=true&forceAspectRatio=true&useTrim=true&size=613x613"
+    ]
+  },
+  {
+    id: 3,
+    name: "Townie Path Go! Damen matte rosewood",
+    category: "e-bike",
+    price: 1240.50,
+    size: {
+      51: 0,
+      53: 0,
+      55: 0,
+      57: 1,
+      58: 1,
+    },
+    images: [
+      "https://images.internetstores.de/products/1076534/02/7bf09e/electra-townie-path-go-5i-275-damen-matte-rosewood-2.jpg?forceSize=true&forceAspectRatio=true&useTrim=true&size=613x613",
+      "https://images.internetstores.de/products/1076534/02/3623bc/electra-townie-path-go-5i-275-damen-matte-rosewood-1.jpg?forceSize=true&forceAspectRatio=true&useTrim=true&size=613x613",
+      "https://images.internetstores.de/products/1076534/02/0703ff/electra-townie-path-go-5i-275-damen-matte-rosewood-3.jpg?forceSize=true&forceAspectRatio=true&useTrim=true&size=613x613"
+    ]
+  },
+  {
+    id: 4,
+    name: "Bear Peak Power 2.0 black/black",
+    category: "mountain",
+    price: 660.50,
+    size: {
+      51: 0,
+      53: 2,
+      55: 0,
+      57: 0,
+      58: 0
+    },
+    images: [
+      "https://images.internetstores.de/products/1005968/02/9beee7/serious-bear-peak-power-20-black-black-2.jpg?forceSize=true&forceAspectRatio=true&useTrim=true&size=613x613",
+      "https://images.internetstores.de/products/1005968/02/ade296/serious-bear-peak-power-20-black-black-1.jpg?forceSize=true&forceAspectRatio=true&useTrim=true&size=613x613",
+      "https://images.internetstores.de/products/1005968/02/c1f3f3/serious-bear-peak-power-20-black-black-3.jpg?forceSize=true&forceAspectRatio=true&useTrim=true&size=613x613"
+    ]
+  },
+  {
+    id: 5,
+    name: "Gravix Pro mantis",
+    category: "road",
+    price: 1799.50,
+    size: {
+      51: 0,
+      53: 0,
+      55: 1,
+      57: 1,
+      58: 1
+    },
+    images: [
+      "https://images.internetstores.de/products/1281882/02/842d91/serious-gravix-pro-mantis-2.jpg?forceSize=true&forceAspectRatio=true&useTrim=true&size=613x613",
+      "https://images.internetstores.de/products/1281882/02/4ce818/serious-gravix-pro-mantis-1.jpg?forceSize=true&forceAspectRatio=true&useTrim=true&size=613x613",
+      "https://images.internetstores.de/products/1281882/02/151eb7/serious-gravix-pro-mantis-3.jpg?forceSize=true&forceAspectRatio=true&useTrim=true&size=613x613"
+    ]
+  },
+  {
+    id: 6,
+    name: "Rockville Disc white/pink",
+    category: "mountain",
+    price: 760.50,
+    size: {
+      51: 1,
+      53: 0,
+      55: 0,
+      57: 1,
+      58: 0,
+    },
+    images: [
+      "https://images.internetstores.de/products/1132004/02/67e335/serious-rockville-275-disc-white-pink-7.jpg?forceSize=true&forceAspectRatio=true&useTrim=true&size=613x613",
+      "https://images.internetstores.de/products/1132004/02/1a29d1/serious-rockville-275-disc-white-pink-1.jpg?forceSize=true&forceAspectRatio=true&useTrim=true&size=613x613",
+      "https://images.internetstores.de/products/1132004/01/dc001d/serious-rockville-275-disc-white-pink-2.jpg?forceSize=true&forceAspectRatio=true&useTrim=true&size=613x613"
+    ]
+  },
+  {
+    id: 7,
+    name: "Fenix SL Disc 105 red",
+    category: "road",
+    price: 2490.50,
+    size: {
+      51: 1,
+      53: 0,
+      55: 0,
+      57: 1,
+      58: 1,
+    },
+    images: [
+      "https://images.internetstores.de/products/1368804/02/678b72/ridley-bikes-fenix-sl-disc-105-red-2.jpg?forceSize=true&forceAspectRatio=true&useTrim=true&size=613x613",
+      "https://images.internetstores.de/products/1368804/02/145031/ridley-bikes-fenix-sl-disc-105-red-1.jpg?forceSize=true&forceAspectRatio=true&useTrim=true&size=613x613",
+      "https://images.internetstores.de/products/1368804/02/77f797/ridley-bikes-fenix-sl-disc-105-red-3.jpg?forceSize=true&forceAspectRatio=true&useTrim=true&size=613x613"
+    ]
+  },
+  {
+    id: 8,
+    name: "Detroit EQ Wave berry",
+    category: "city",
+    price: 340.50,
+    size: {
+      51: 0,
+      53: 1,
+      55: 0,
+      57: 2,
+      58: 1, 
+    },
+    images: [
+      "https://images.internetstores.de/products/1124297/02/04ffc9/ortler-detroit-eq-damen-berry-7.jpg?forceSize=true&forceAspectRatio=true&useTrim=true&size=613x613",
+      "https://images.internetstores.de/products/1124297/02/91a7a0/ortler-detroit-eq-damen-berry-1.jpg?forceSize=true&forceAspectRatio=true&useTrim=true&size=613x613",
+      "https://images.internetstores.de/products/1124297/01/890527/ortler-detroit-eq-damen-berry-2.jpg?forceSize=true&forceAspectRatio=true&useTrim=true&size=613x613"
+    ]
+  }
+]
